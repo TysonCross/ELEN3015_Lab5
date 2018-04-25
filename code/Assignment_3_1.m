8ff15e94a0d758f8f9ffc9fb730328ae05acab8e
Tyson <tyson.cross@gmail.com>
2018-04-25 20:32:23 +0200

    Question 3.1.4

--- code/Assignment_3_1.m
+++ code/Assignment_3_1.m
@@ -3,11 +3,6 @@
 % addpath('utilities');
 clc; clear all;
 
-% ELEN3015 Lab5 Assignment 3.1
-% Tyson Cross 1239448
-% addpath('utilities');
-clc; clear all;
-
 
 
 %% Question 1:
@@ -24,7 +19,7 @@ G1 = [ 	1     1     0     1     0     0     0 ;...
         1     0     1     0     0     0     1 ];
 
 % codeword
-u = [1 0 1 1];      % corresponds to polynomial (1 + x^2 + x^3)
+u = [1 0 0 1];      % corresponds to polynomial (1 + x^3)
 
 % C1
 C1 = mod(u*G1,2);
@@ -32,8 +27,9 @@ C1 = mod(u*G1,2);
 % output
 disp('H ='); disp(H1);
 disp('G ='); disp(G1);
-disp(['Input = ', num2str(u)]);  disp(' ');
-disp([' C1   = ', num2str(C1)]); disp(' ');
+disp(['Input (u) = ', num2str(u)]);  disp(' ');
+disp('C1 = uG'); disp(' ');
+disp(['C1 = ', num2str(C1)]); disp(' ');
 disp('--------------------------------------------------------');disp(' ');
 %{
 Comment:
@@ -53,51 +49,94 @@ assert(k== length(u));
 
 
 % parity check and generator matrices
+disp([' g(x) = ', num2str(p) ]); disp(' ');
+fprintf(' g(x) = '); gfpretty(p); disp(' ');
 disp('Generating the H and G parity and generator matrices...'); disp(' ');
 H2 = parityMatrix(m,p);
 G2 = generatorMatrix(H2);
 
 C2 = mod(u*G2,2);
 
-disp([' g(x) = ', num2str(p) ]); disp(' ');
-disp(['Input = ', num2str(u)]);  disp(' ');
-disp(['  C2  = ', num2str(C2)]); disp(' ');
+% output
+disp('H ='); disp(H2);
+disp('G ='); disp(G2);
+disp(['Input (u) = ', num2str(u)]);  disp(' ');
+disp('C2 = uG'); disp(' ');
+disp(['C2 = ', num2str(C2)]); disp(' ');
 
 %% Note:
-disp('*NB*'); disp(' ');
+disp('*Note'); disp(' ');
 
 C_mult = gfconv(u,p);
 [~,C_div] = gfdeconv(u,p);
-disp(' (1 + X^2 + X^3) · (1 + X + X^3) = '); gfpretty(C_mult);
+
+% output
+disp(' ');
+fprintf(' (1 + X^3) · (1 + X + X^3) = '); gfpretty(C_mult);
 disp (' ');
-disp(['As a binary row vector: ', num2str(C_mult)]); disp(' ')
-disp('But (1 + X + X^2  + X^3  + X^4  + X^5  + X^6) mod (1 + X + X^3) = '); gfpretty(C_div);
+disp(['As a binary row vector: ', num2str(C_mult)]); disp(' ');
+disp(' ');
+fprintf(' (1 + X^3) ÷ (1 + X + X^3) = '); gfpretty(C_div);
 disp (' '); 
-disp(['As a binary row vector: ', num2str(C_div)]); disp(' ')
+disp(['As a binary row vector: ', num2str(C_div)]); disp(' ');
+disp('Both of these solutions are equivalent under Galois Field G(3)');
+disp(' ')
 disp('--------------------------------------------------------');disp(' ');
 %{
 Comment:
- C is the result of polynomial multiplication between the two row vectors,
- in the Galois field G(3): (1 + x^2 + x^3)(1 + x + x^3)(1 + x^4) = 1 + x + x^4 + x^6
+Multiplication is the same as division:
+ The result of polynomial multiplication between two row vectors,
+ in the Galois field G(3): (1 + x^3)(1 + x + x^3) = 1 + x + x^4 + x^6
 = [1 1 0 0 1 0 1]
-{This can be reduced via modulus (1 + x + x^3) }
-= (x)x^3 + (x^3)(x^3) = 0
+{This is reduced via modulo (1 + x + x^3)}
+= x = 0 1
 %}
 
 %% Question 3:
-
 disp('Question 3:');  disp(' ');
+
+% output
+disp(['C1 = ', num2str(C1)]); disp(' ');
+disp(['C2 = ', num2str(C2)]); disp(' ');
 if isequal(C1,C2)
     disp([   'C1 and C2 are the same, because the input code word, ',...
             'and generator matrices are identical.']);
   	disp([  'The irreducible polynomial 1+x+x^3 creates the parity ',...
             'and generator matrices shown in Handout equation (2.6)']);
+        disp([  'The first m (three) bits are the parity bits,',...
+            'and the next four bits are the original message (u)']);
 else
     disp('C1 and C2 do not match')
 end
+disp(' ');
 disp('--------------------------------------------------------');disp(' ');
 
 
 %% Question 4:
+disp('Question 4:');  disp(' ');
 
-m_x = u; % [1 0 0 1] = 
+x_m = [0 0 0 1];                % x^(n-k) = x^m = [0 0 0 1]
+M_x = gfconv(u,x_m);            % previous codeword u = [1 0 0 1]
+
+[~,P_x] = gfdeconv(M_x,p);       	% Parity bits: m(x)*x^3 / g(x)
+C3 = bitxor(zeropad(P_x,length(M_x),'after'),M_x);
+
+% output
+disp(['Input = ', num2str(u)]);  disp(' ');
+fprintf('g(x) = '); gfpretty (p); disp(' ');
+disp(['g(x) as a binary row vector: ', num2str(p)]); disp(' ')
+disp(' ');
+fprintf('m(x)*X^(n-k) = '); gfpretty (M_x); disp(' ');
+disp(['m(x)*X^(n-k) as a binary row vector: ', num2str(M_x)]); disp(' ')
+disp(' ');
+fprintf('P(x) = m(x)(X^(n-k))/g(x) = '); gfpretty(P_x); disp(' ');
+disp(['P(x) as a binary row vector: ', num2str(P_x)]); disp(' ')
+disp(' ');
+disp('C3 =  m(x) + P(x)  *Note: P(x) is zero-padded to the length of m(x)');
+disp(' ');
+fprintf('C3 = '); gfpretty(C3); disp(' ');
+disp(['C3 as a binary row vector: ', num2str(C3)]); disp(' ')
+disp('--------------------------------------------------------');disp(' ');
+
+
+
---
+++ code/zeropad.m
@@ -0,0 +1,25 @@
+function [padded_array] = zeropad(array_to_be_padded, len, pos)
+%zeropad() pads an numeric array with zeros to be specific length
+
+if nargin < 3
+    pos = 'front';
+end
+
+if len<=length(array_to_be_padded)
+    error('Padding not possible')
+end
+
+padded_array = [array_to_be_padded];
+
+for i=1:len-length(array_to_be_padded)
+    if strcmp(pos,'front') || strcmp(pos,'prefix') || strcmp(pos,'before')
+        padded_array = [0 padded_array];
+    elseif strcmp(pos,'behind') || strcmp(pos,'suffix') || strcmp(pos,'after')
+        padded_array = [padded_array 0];
+    else
+        error('Padding position not recognised')
+    end
+end
+
+end
+
