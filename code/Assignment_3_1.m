6ff231725b3318b2d4651b106c969d782e65db85
Tyson <tyson.cross@gmail.com>
2018-04-25 16:49:58 +0200

    First three questions answered

--- code/Assignment_3_1.m
+++ code/Assignment_3_1.m
@@ -11,41 +11,29 @@ clc; clear all;
 
 
 %% Question 1:
-% setup
-m = 3;          % number of parity bits
-p = [1 1 0 1];  % irreducible polynomial corresponding to m: 1 + x + x^3
+disp('Question 1:'); disp(' ');
 
-% length of codeword, information and parity bits
-n = 2^m - 1;
-k = n - m;
+% The matrices from the Handout equation (2.6)
+H1 = [  1     0     0     1     0     1     1 ;...
+        0     1     0     1     1     1     0 ;...
+        0     0     1     0     1     1     1 ];
 
-% parity check and generator matrices
-% The resulting matrices are the same as Handout equation (2.6)
-H = parityMatrix(m,p);
-G = generatorMatrix(H);
-%{
-H = [   1     0     0     1     0     1     1;...
-        0     1     0     1     1     1     0;...
-        0     0     1     0     1     1     1];
-
-G = [   1     1     0     1     0     0     0;...
-        0     1     1     0     1     0     0;...
-        1     1     1     0     0     1     0;...
-        1     0     1     0     0     0     1];
-%}
+G1 = [ 	1     1     0     1     0     0     0 ;...
+        0     1     1     0     1     0     0 ;...
+        1     1     1     0     0     1     0 ;...
+        1     0     1     0     0     0     1 ];
 
 % codeword
-u = [1 0 0 1];  % corresponds to polynomial (1 + x^4)
+u = [1 0 1 1];      % corresponds to polynomial (1 + x^2 + x^3)
 
 % C1
-C1 = mod(u*G,2);
+C1 = mod(u*G1,2);
 
 % output
-disp('Question 1:'); disp(' ');
-disp('H ='); disp(H);
-disp('G ='); disp(G);
-disp('Input codeword ='); disp(u);
-disp('C1 ='); disp(C1);
+disp('H ='); disp(H1);
+disp('G ='); disp(G1);
+disp(['Input = ', num2str(u)]);  disp(' ');
+disp([' C1   = ', num2str(C1)]); disp(' ');
 disp('--------------------------------------------------------');disp(' ');
 %{
 Comment:
@@ -54,33 +42,62 @@ Comment:
 %}
 
 %% Question 2:
-% setup
-q = [1 1 0 1];  % irreducible polynomial: 1 + x + x^3
-C2 =  gfconv(u,q);
-C2_mod = gftuple(C2,3)
+disp('Question 2:'); disp(' ');
 
-disp('Question 2:'); 
-disp('Input codeword ='); disp(u); gfpretty(u);
-disp('p(x) ='); disp(q); gfpretty(q);
-disp('C2 ='); disp(C2); gfpretty(C2)
-disp('C2 (simple) ='); disp(C2_mod); gfpretty(C2_mod)
+p = [1 1 0 1];      % irreducible polynomial: 1 + x + x^3
+
+m = 3;           	% number of parity bits
+n = 2^m - 1;        % length of codeword
+k = n - m;        	% length of message
+assert(k== length(u));
+
+
+% parity check and generator matrices
+disp('Generating the H and G parity and generator matrices...'); disp(' ');
+H2 = parityMatrix(m,p);
+G2 = generatorMatrix(H2);
+
+C2 = mod(u*G2,2);
+
+disp([' g(x) = ', num2str(p) ]); disp(' ');
+disp(['Input = ', num2str(u)]);  disp(' ');
+disp(['  C2  = ', num2str(C2)]); disp(' ');
+
+%% Note:
+disp('*NB*'); disp(' ');
+
+C_mult = gfconv(u,p);
+[~,C_div] = gfdeconv(u,p);
+disp(' (1 + X^2 + X^3) · (1 + X + X^3) = '); gfpretty(C_mult);
+disp (' ');
+disp(['As a binary row vector: ', num2str(C_mult)]); disp(' ')
+disp('But (1 + X + X^2  + X^3  + X^4  + X^5  + X^6) mod (1 + X + X^3) = '); gfpretty(C_div);
+disp (' '); 
+disp(['As a binary row vector: ', num2str(C_div)]); disp(' ')
 disp('--------------------------------------------------------');disp(' ');
 %{
 Comment:
- C2 is the result of polynomial multiplication between the two row vectors,
- in the Galois field G(3): (1 + x + x^3)(1 + x^4) = 1 + x + x^4 + x^6
+ C is the result of polynomial multiplication between the two row vectors,
+ in the Galois field G(3): (1 + x^2 + x^3)(1 + x + x^3)(1 + x^4) = 1 + x + x^4 + x^6
 = [1 1 0 0 1 0 1]
 {This can be reduced via modulus (1 + x + x^3) }
 = (x)x^3 + (x^3)(x^3) = 0
 %}
 
 %% Question 3:
-disp('Question 2:');  disp(' ');
-disp('???')
+
+disp('Question 3:');  disp(' ');
+if isequal(C1,C2)
+    disp([   'C1 and C2 are the same, because the input code word, ',...
+            'and generator matrices are identical.']);
+  	disp([  'The irreducible polynomial 1+x+x^3 creates the parity ',...
+            'and generator matrices shown in Handout equation (2.6)']);
+else
+    disp('C1 and C2 do not match')
+end
 disp('--------------------------------------------------------');disp(' ');
 
 
 %% Question 4:
 
 m_x = u; % [1 0 0 1] = 
-P_x = 
